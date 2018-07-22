08fa22706501812c677dd55b9dd873ded695bb0f
Jaakko <jaakko@kronogra.fi>
2018-07-22 17:49:27 +0300

    Initialized the project
    Also made basic features:
    Movement
    Base
    Oxygen

---
+++ ProjectHeron/Assets/Scripts/playerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerScript : MonoBehaviour {
+
+    public bool oxygenAttacehd;
+    private Image oxygenBar;
+    private GameObject oxygenBarObject;
+    [SerializeField]
+    float oxygenRegen;
+    [SerializeField]
+    float oxygenLost;
+	void Start () {
+        oxygenBarObject = GameObject.Find("oxygenBar");
+        if(oxygenBarObject)
+        oxygenBar = oxygenBarObject.GetComponent<Image>();
+	}
+	
+	void Update () {
+        if (oxygenBar)
+        {
+            if (oxygenAttacehd)
+            {
+                if(oxygenBar.rectTransform.sizeDelta.x < 200)
+                    oxygenBar.rectTransform.sizeDelta = new Vector2(oxygenBar.rectTransform.sizeDelta.x + oxygenRegen, oxygenBar.rectTransform.sizeDelta.y);
+            }
+            else
+            {
+                if (oxygenBar.rectTransform.sizeDelta.x > 0)
+                    oxygenBar.rectTransform.sizeDelta = new Vector2(oxygenBar.rectTransform.sizeDelta.x - oxygenLost, oxygenBar.rectTransform.sizeDelta.y);
+            }
+        }
+	}
+}
