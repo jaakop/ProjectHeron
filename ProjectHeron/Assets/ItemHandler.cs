6bcb11dad8c2c85009a80447237c4d175058e0ac
Jaakko <jaakko@kronogra.fi>
2018-08-05 01:28:48 +0300

    Added a item and fixed a bug
    fixed that the oxygen tether wouldnt show
    
    Added a item
    Coded item handler

---
+++ ProjectHeron/Assets/ItemHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ItemHandler : MonoBehaviour {
+
+    public bool isHold;
+    private bool insideObject;
+    [SerializeField]
+    private float moveSpeed;
+	// Use this for initialization
+	void Start () {
+        isHold = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (isHold)
+        {
+            if (cursorPosition.x - transform.position.x > 0.025 || cursorPosition.x - transform.position.x < -0.025)
+            {
+                if (cursorPosition.y - transform.position.y > 0.025 || cursorPosition.y - transform.position.y < -0.025)
+                {
+                    Vector2 vector = new Vector2(cursorPosition.x - transform.position.x, cursorPosition.y - transform.position.y);
+                    vector.Normalize();
+                    transform.position = new Vector2(transform.position.x + vector.x * moveSpeed, transform.position.y + vector.y * moveSpeed);
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && insideObject)
+        {
+            if (!isHold)
+                isHold = true;
+            else
+                isHold = false;
+        }
+	}
+    private void OnMouseEnter()
+    {
+        insideObject = true;
+    }
+
+    private void OnMouseExit()
+    {
+        insideObject = false;
+    }
+}
